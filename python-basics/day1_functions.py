29900eef3a60ef5b7fb79cf916d425844f362632
Shabnam <shabnam.shirzadegan@gmail.com>
2026-06-11 22:22:26 -0700

    Exercise 3: Reverse a string

--- python-basics/day1_functions.py
+++ python-basics/day1_functions.py
@@ -8,7 +8,6 @@ def average(nums):
     # ave = sum(nums) / len(nums) 
     return ave
 
-
 # ---- Exercise 2: Find largest number ----
 def find_max(nums):
      max = nums[0]
@@ -17,10 +16,18 @@ def find_max(nums):
                max = nums[i+1]
      return max
 
+# ---- Exercise 3: Reverse a string  ----
+def string_reverser(s):
+     reversed_string = ""
+     for i in range(-1, -len(s)-1, -1):
+          reversed_string += s[i]
+     return reversed_string
 
+     
 
 
 
 if __name__ == "__main__":
     print("Exercise1_output:", average([10, 20, 30]))
     print("Exercise2_output:", find_max([3, 7, 2, 9, 1]))
+    print("Exercise3_output:", string_reverser("hello"))
