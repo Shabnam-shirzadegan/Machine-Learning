8df4426d55fd4e6cf9c0c5335553be79e2733644
Shabnam <shabnam.shirzadegan@gmail.com>
2026-06-11 21:53:21 -0700

    change files

--- python-basics/day1_functions.py
+++ python-basics/day1_functions.py
@@ -1,3 +1,4 @@
+# ---- Exercise 1: Find average ----
 def average(nums):
     sum = 0
     for i in range(len(nums)):
@@ -6,4 +7,20 @@ def average(nums):
     # built-in sum() function
     # ave = sum(nums) / len(nums) 
     return ave
-print(average([10, 20, 30]))
+
+
+# ---- Exercise 2: Find largest number ----
+def find_max(nums):
+     max = nums[0]
+     for i in range(len(nums) -1):
+          if max < nums[i+1]:
+               max = nums[i+1]
+     return max
+
+
+
+
+
+if __name__ == "__main__":
+    print("Exercise1_output:", average([10, 20, 30]))
+    print("Exercise2_output:", find_max([3, 7, 2, 9, 1]))
