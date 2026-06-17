ae4667a7c05919ee2ca6f7a0da5bb594d6f66fdb
Shabnam <shabnam.shirzadegan@gmail.com>
2026-06-17 15:01:48 -0700

    Day 2 - list comprehension: squares

---
+++ python-basics/day2_functions.py
@@ -0,0 +1,11 @@
+# ---- Exercise 1: List comprehension - squares ----
+def squares(nums):
+   return [num**2 for num in nums]
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("Exercise1_output:", squares([1, 2, 3, 4, 5]))
