622e818db15571011a54691a01b9c2e233038afc
Shabnam <shabnam.shirzadegan@gmail.com>
2026-06-17 15:23:42 -0700

    Day 2 - list comprehension: flatten nested list

--- python-basics/day2_functions.py
+++ python-basics/day2_functions.py
@@ -10,6 +10,9 @@ def long_words(l):
 def c_to_f(num):
     return [C * 9/5 + 32 for C in num]
 
+# ---- Exercise 4: List comprehension - flatten a nested list ----
+def flatter(l):
+    return [item for sublist in l for item in sublist]
 
 
 
@@ -17,3 +20,4 @@ if __name__ == "__main__":
     print("Exercise1_output:", squares([1, 2, 3, 4, 5]))
     print("Exercise2_output:", long_words(["cat", "elephant", "dog", "python", "hi"]))
     print("Exercise3_output:", c_to_f([0, 20, 37, 100]))
+    print("Exercise4_output:", flatter([[1, 2], [3, 4], [5, 6]]))
